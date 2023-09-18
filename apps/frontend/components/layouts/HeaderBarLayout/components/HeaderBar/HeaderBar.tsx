cc3a6f84828d0c929d51742275df9ef2271bbfed
rizerkrof <34008166+rizerkrof@users.noreply.github.com>
2023-09-18 20:31:22 +0200

    feat: show login button if not connected

--- apps/frontend/components/layouts/HeaderBarLayout/components/HeaderBar/HeaderBar.tsx
+++ apps/frontend/components/layouts/HeaderBarLayout/components/HeaderBar/HeaderBar.tsx
@@ -2,17 +2,22 @@ import Link from 'next/Link';
 
 import { ContentBoxLayout } from 'components/layouts/ContentBoxLayout';
 import { Pages } from 'constant';
+import { useIsUserConnected } from 'services/api/auth/useIsUserConnected';
 
 import style from './HeaderBar.module.css';
 
 export const HeaderBar = (): JSX.Element => {
+  const isUserConnected = useIsUserConnected();
+
   return (
     <ContentBoxLayout>
       <div className={style.div}>
         <h2 className={style.title}>Nest.js feature flags app!</h2>
-        <Link className={style.loginButton} href={Pages.Login}>
-          <p className={style.p}>Login</p>
-        </Link>
+        {!isUserConnected && (
+          <Link className={style.loginButton} href={Pages.Login}>
+            <p className={style.p}>Login</p>
+          </Link>
+        )}
       </div>
     </ContentBoxLayout>
   );
