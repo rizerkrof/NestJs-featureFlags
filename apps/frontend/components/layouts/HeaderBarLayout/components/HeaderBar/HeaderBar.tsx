90f02f1ab59752a3e30234d819815960b4a3ade3
rizerkrof <34008166+rizerkrof@users.noreply.github.com>
2023-09-18 19:58:25 +0200

    feat: display login button on home page

--- apps/frontend/components/layouts/HeaderBarLayout/components/HeaderBar/HeaderBar.tsx
+++ apps/frontend/components/layouts/HeaderBarLayout/components/HeaderBar/HeaderBar.tsx
@@ -1,11 +1,19 @@
+import Link from 'next/Link';
+
 import { ContentBoxLayout } from 'components/layouts/ContentBoxLayout';
+import { Pages } from 'constant';
 
 import style from './HeaderBar.module.css';
 
-export const HeaderBar = (): JSX.Element => (
-  <ContentBoxLayout>
-    <div>
-      <h2 className={style.title}>Nest.js feature flags app!</h2>
-    </div>
-  </ContentBoxLayout>
-);
+export const HeaderBar = (): JSX.Element => {
+  return (
+    <ContentBoxLayout>
+      <div className={style.div}>
+        <h2 className={style.title}>Nest.js feature flags app!</h2>
+        <Link className={style.loginButton} href={Pages.Login}>
+          <p className={style.p}>Login</p>
+        </Link>
+      </div>
+    </ContentBoxLayout>
+  );
+};
