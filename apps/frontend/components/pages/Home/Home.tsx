54cba3bae0ad5c4ab4ecdbc02cbc38c6768bc781
rizerkrof <34008166+rizerkrof@users.noreply.github.com>
2023-09-20 14:40:04 +0200

    feat: conditions code based on the feature flag

--- apps/frontend/components/pages/Home/Home.tsx
+++ apps/frontend/components/pages/Home/Home.tsx
@@ -2,16 +2,23 @@ import Link from 'next/link';
 
 import { ContentBoxLayout } from 'components/layouts/ContentBoxLayout';
 import { Pages } from 'constant';
+import { useIsFeatureActive } from 'services/api/feature/useIsFeatureActive';
 
 import style from './Home.module.css';
 
-export const Home = (): JSX.Element => (
-  <ContentBoxLayout>
-    <div className={style.div}>
-      <h1 className={style.title}>The HOME page</h1>
-      <Link className={style.button} href={Pages.NewFeature}>
-        <p className={style.p}>New Feature in progress</p>
-      </Link>
-    </div>
-  </ContentBoxLayout>
-);
+export const Home = (): JSX.Element => {
+  const isNewFeatureActive = useIsFeatureActive('NEW_FEATURE');
+
+  return (
+    <ContentBoxLayout>
+      <div className={style.div}>
+        <h1 className={style.title}>The HOME page</h1>
+        {isNewFeatureActive && (
+          <Link className={style.button} href={Pages.NewFeature}>
+            <p className={style.p}>New Feature in progress</p>
+          </Link>
+        )}
+      </div>
+    </ContentBoxLayout>
+  );
+};
--- apps/frontend/pages/new-feature.tsx
+++ apps/frontend/pages/new-feature.tsx
@@ -1,9 +1,20 @@
+import DefaultErrorPage from 'next/error';
+
 import { HeaderBarLayout } from 'components/layouts/HeaderBarLayout';
 import { NewFeature } from 'components/pages';
+import { useIsFeatureActive } from 'services/api/feature/useIsFeatureActive';
 
-const NewFeaturePage = () => (
-  <HeaderBarLayout>
-    <NewFeature />
-  </HeaderBarLayout>
-);
+const NewFeaturePage = () => {
+  const isNewFeatureActive = useIsFeatureActive('NEW_FEATURE');
+
+  if (!isNewFeatureActive) {
+    return <DefaultErrorPage statusCode={404} />;
+  }
+
+  return (
+    <HeaderBarLayout>
+      <NewFeature />
+    </HeaderBarLayout>
+  );
+};
 export default NewFeaturePage;
